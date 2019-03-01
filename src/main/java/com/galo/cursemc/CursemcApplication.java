bf32b84447cd18cdb6c5779bfc67c6d245402a5b
lucasGalo <galo_pc@outlook.com>
2019-02-28 21:14:12 -0300

    Produto e associacao

--- src/main/java/com/galo/cursemc/CursemcApplication.java
+++ src/main/java/com/galo/cursemc/CursemcApplication.java
@@ -8,13 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.galo.cursemc.domain.Categoria;
+import com.galo.cursemc.domain.Produto;
 import com.galo.cursemc.repositories.CategoriaRepository;
+import com.galo.cursemc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursemcApplication implements CommandLineRunner{
 
 	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursemcApplication.class, args);
 	}
@@ -24,7 +30,19 @@ public class CursemcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "IMPRESSORA", 800.00);
+		Produto p3 = new Produto(null, "MOUSE", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 
